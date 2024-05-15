a9ea9da853db42d6f165b558f97a6c2236be9916
stfnrosa <rosaa.estefanii@gmail.com>
2024-05-14 21:30:05 -0300

    tratamento de erros

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -18,6 +18,10 @@ async function criaVideo(titulo, descricao, url, imagem) {
         })
     });
 
+    if(!conexao.ok){
+        throw new Error("Não foi possível enviar o vídeo");
+    }
+
     const conexaoConvertida = await conexao.json();
     return conexaoConvertida;
 }
